8259c9291db76e178f0add7280afe4d8ca09d4d9
Rohit3901 <mayanksaket80@gmail.com>
2019-08-14 10:41:01 +0530

    updated main activity

--- app/src/main/java/com/example/imageloader/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/imageloader/ui/main/MainActivity.kt
@@ -12,35 +12,6 @@ import com.example.imageloader.ui.main.photos.PhotoAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
-/*
-
-class MainActivity : AppCompatActivity() {
-
-    private val imageList = arrayListOf(
-        "https://images.pexels.com/photos/1722183/pexels-photo-1722183.jpeg?dl&fit=crop&crop=entropy&w=1280&h=1920",
-        "https://images.pexels.com/photos/1667580/pexels-photo-1667580.jpeg?dl&fit=crop&crop=entropy&w=1280&h=1920",
-        "https://images.pexels.com/photos/1470405/pexels-photo-1470405.jpeg?dl&fit=crop&crop=entropy&w=1280&h=853",
-        "https://images.pexels.com/photos/1005417/pexels-photo-1005417.jpeg?dl&fit=crop&crop=entropy&w=1280&h=1600",
-        "https://images.pexels.com/photos/1294671/pexels-photo-1294671.jpeg?dl&fit=crop&crop=entropy&w=1280&h=1920",
-        "https://images.pexels.com/photos/1040893/pexels-photo-1040893.jpeg?dl&fit=crop&crop=entropy&w=1280&h=1919",
-        "https://images.pexels.com/photos/1956974/pexels-photo-1956974.jpeg?dl&fit=crop&crop=entropy&w=1280&h=853",
-        "https://images.pexels.com/photos/1374064/pexels-photo-1374064.jpeg?dl&fit=crop&crop=entropy&w=1280&h=1706",
-        "https://images.pexels.com/photos/1931142/pexels-photo-1931142.jpeg?dl&fit=crop&crop=entropy&w=1280&h=1919",
-        "https://images.pexels.com/photos/1295036/pexels-photo-1295036.jpeg?dl&fit=crop&crop=entropy&w=1280&h=1707",
-        "https://images.pexels.com/photos/1320684/pexels-photo-1320684.jpeg?dl&fit=crop&crop=entropy&w=1920&h=1440",
-        "https://images.pexels.com/photos/1908677/pexels-photo-1908677.jpeg?dl&fit=crop&crop=entropy&w=1280&h=1706"
-    )
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        recyclerView?.layoutManager = GridLayoutManager(this, 2)
-        recyclerView?.adapter = ImageAdapter(imageList, this)
-
-    }
-}
-*/
-
 class MainActivity :BaseActivity<MainViewModel>(){
 
 
@@ -66,7 +37,6 @@ class MainActivity :BaseActivity<MainViewModel>(){
 
     override fun setupObservers() {
         super.setupObservers()
-
         viewModel.loading.observe(this, Observer {
             progressBar.visibility = if (it) View.VISIBLE else View.GONE
         })
